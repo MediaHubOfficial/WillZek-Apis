0bb944f181e880a71d5573120c18e1a70324849a
TheWolfCode <machayrodriguezvierin88@gmail.com>
2025-03-29 20:29:23 -0500

    Update ytmp3.js

--- ytmp3.js
+++ ytmp3.js
@@ -14,7 +14,9 @@ export default async function handler(req, res) {
     // Modificar el campo "creator" a "willzek"
     data.creator = "willzek";
 
-    res.status(response.status).json(data);
+    // Enviar con formato “bonito”
+    res.setHeader('Content-Type', 'application/json');
+    res.status(response.status).send(JSON.stringify(data, null, 2));
   } catch (error) {
     res.status(500).json({ error: "Error al obtener los datos", details: error.message });
   }
